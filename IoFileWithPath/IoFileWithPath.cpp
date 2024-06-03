499577bd608500de0d79da97e096ac5adf7c1832
Qilqilll <aqilf612@gmail.com>
2024-06-03 10:15:46 +0700

    while loop and outfile

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -12,4 +12,17 @@ int main() {
 
 	ofstream outfile;
 	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis File, \'q\' untuk Keluar" << endl;
+
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	outfile.close();
 }
