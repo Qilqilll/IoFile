e769671c32cb65599c7087fef0fa5c7a638b9a36
Qilqilll <aqilf612@gmail.com>
2024-06-03 10:38:35 +0700

    try and catch exception

--- Manualexception/Manualexception.cpp
+++ Manualexception/Manualexception.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <exception>
 using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat belajar di prodi TI UMY" << endl;
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+}
