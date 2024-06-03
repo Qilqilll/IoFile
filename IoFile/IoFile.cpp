9c2e27bf9e40d80d995ed2d4317406c17efe1888
Qilqilll <aqilf612@gmail.com>
2024-06-03 09:52:57 +0700

    unlimited loop

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -3,3 +3,12 @@
 #include <string>
 using namespace std;
 
+int main() {
+	string baris;
+
+	ofstream outfile;
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis File, \'q\q untuk Keluar " << endl;
+
+	while (true) {
